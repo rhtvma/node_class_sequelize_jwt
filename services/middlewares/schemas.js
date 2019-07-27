a6497ae09b4f8399d25ffc36fc15b654f85c7b39
Rohit <rohit114355@gmail.com>
2019-07-27 21:04:15 +0530

    Joi Implementation for signup and signin routes

--- services/middlewares/schemas.js
+++ services/middlewares/schemas.js
@@ -2,23 +2,9 @@
  * Created by rohit on 07/26/2019.
  * https://scotch.io/tutorials/node-api-schema-validation-with-joi
  */
-
 const Joi = require('@hapi/joi');
 
-// const schema = Joi.object()
-//     .keys(
-//         {
-//             name: Joi.string().alphanum().min(3).max(30),
-//             email: Joi.string().email({minDomainSegments: 2}).required(),
-//             password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
-//             role: Joi.string().alphanum().min(3).max(30),
-//             access_token: [Joi.string(), Joi.number()],
-//             // birthyear: Joi.number().integer().min(1900).max(2013)required(),
-//         }
-//     ).without('name', 'role');
-
-// accepts name only as letters and converts to lowercase
-const name = Joi.string().alphanum().min(3).max(30).lowercase();
+const name = Joi.string().alphanum().min(3).max(30).lowercase();// accepts name only as letters and converts to lowercase
 const email = Joi.string().email({minDomainSegments: 2}).lowercase();
 
 // accepts a valid UUID v4 string as id
@@ -32,31 +18,29 @@ const ageSchema = Joi.alternatives().try([
     Joi.string().replace(/^([7-9]|[1-9]\d+)(y|yr|yrs)?$/i, '$1').required()
 ]);
 
-const userDataSchema = Joi.object().keys({
+const signupSchema = Joi.object().keys({
     name: name.required(),
     email: email.required(),
-    password: Joi.string().min(5).required(),
+    password: Joi.string().min(5).required(),// password and confirmPassword must contain the same value
+    confirmPassword: Joi.string().valid(Joi.ref('password')).required().strict(),
     role: Joi.string().valid('user', 'admin').uppercase(),
     sex: Joi.string().valid(['M', 'F', 'MALE', 'FEMALE']).uppercase(),
-    // if type is STUDENT, then age is required
-    age: Joi.when('type', {
+    age: Joi.when('type', {// if type is STUDENT, then age is required
         is: 'user',
         then: ageSchema, //.required(),
         otherwise: ageSchema
     })
 })
 
-// password and confirmPassword must contain the same value
-const authDataSchema = Joi.object({
-    // id: userID.required(),
+
+const signinSchema = Joi.object({
     email: Joi.string().email().lowercase().required(),
-    password: Joi.string().min(5).required(),
-    // confirmPassword: Joi.string().valid(Joi.ref('password')).required().strict()
+    password: Joi.string().min(5).required()
 });
 
 
 // export the schemas
 module.exports = {
-    '/signup': userDataSchema,
-    '/signin': authDataSchema
+    '/signup': signupSchema,
+    '/signin': signinSchema
 };
